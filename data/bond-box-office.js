240685fa3687747c129b40c0c4ff6a27e9b3e6f1
Valerie Smith <valeriesmith450@gmail.com>
2021-03-16 13:41:17 -0700

    testing

--- data/bond-box-office.js
+++ data/bond-box-office.js
@@ -1,19 +1,14 @@
 {
-	"title": "Bond Films by Box Office Totals",
+	"title": "Valerie's favorite movies",
 	
 	"films":[
              {
-				"Film":23,
-				"Title":"Skyfall",
-				"Year":2012,
-				"Director":"Sam Mendes",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and John Logan",
-				"Composer":"Thomas Newman",
-				"Bond":"Daniel Craig",
-				"Budget":"$230,000,000.00",
-				"BoxOffice":"$1,110,526,981.00",
-				"Image":"skyfall.jpg"
+				"Film":1,
+				"Title":"Split",
+				"Year":2016,
+				"Director":"M. Night Shyamalan",
+				"Producers":"Jason Blum, Marc Bienstock",
+				"Image":"Split.jpeg"
 			  },
               {
 				"Film":24,
